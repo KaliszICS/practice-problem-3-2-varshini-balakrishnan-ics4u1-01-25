ff2e3eabbb9eecb90f6f434e39ae6e6851107188
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-04 19:51:19 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,9 +7,6 @@ public class PracticeProblem {
 		if (num < 3) {
 			return -1;
 		}
-		if (num ==1) {
-			return 3;
-		}
-		return 2 * towerOfHanoi(num + 1) - 1;
+		return (int) Math.pow(2, num) - 1;
 	}
 }
